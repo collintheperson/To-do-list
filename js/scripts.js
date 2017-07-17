881ba28b1722356380cbb0ecce0a266838bba670
Collin Person and Kate Mashokha <collin+kate@>
2017-07-17 15:09:09 -0700

    add remove button and front end design

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,57 @@ function Task(name, description, deadline)  {
   this.description = description;
   this.deadline = deadline;
 }
-  Task.prototype.showTask() {
-    return "Task: " + this.name + " needs to be completed by " + this.deadline + ".<br>Reminder " + this.description;
-  };
+  Task.prototype.showTask = function() {
+    return "Task: " + this.name + " needs to be completed by " + this.deadline + ". Reminder: " + this.description;
+}
 
 //user interface logic
-  (function() {
-    $("#new-task").submit(function(event)  {
+  $(document).ready(function(){
+    $("form#new-task").submit(function(event)  {
       event.preventDefault();
 
-    var inputtedName = ("new-name").val();
-    var inputtedDescription = ("#new-description").val();
-    var newDeadline = ("#new-deadline").val();
+    var inputtedName = $("input#new-name").val();
+    var inputtedDescription = $("input#new-description").val();
+    var inputtedDeadline = $("input#new-deadline").val();
+    console.log(inputtedName + " " + inputtedDescription + " " + inputtedDeadline);
+    var newTask = new Task (inputtedName, inputtedDescription, inputtedDeadline);
+    $("ul#tasks").append("<li><span class='taskName'>" + newTask.name + "</span></li>");
+
+
+    $(".taskName").last().click(function()  {
+    $("#show-tasks").show();
+    $("#show-tasks h4").text(newTask.showTask());
+    var clicked = $(this);
+
+
+    $("#removeButton").click(function(){
+      // alert("The button is clicked");
+      // $(clicked).child().remove();
+        //alert(clicked);
+        clicked.remove();
+
+
 
     });
+
+
+
+  });
+
+
+
+
+    $("input#new-name").val("");
+    $("input#new-description").val("");
+    $("input#new-deadline").val("");
+
+
+    });
+
+      //$("ul#tasks").child().hide();
+      // $(".h1").click(function(event) {
+      //   alert("Button is clicked");
+      // });
+
+
   });
